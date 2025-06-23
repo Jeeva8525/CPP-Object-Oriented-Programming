de3716b3664113484c1fb0bf602c435979550fb5
Jeeva8525 <jeevananthan8507@gmail.com>
2025-06-23 12:28:20 +0530

    Swapping variables using binary operator

---
+++ binary.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+template<typename T>
+void swapUsingBinary(T& a,T& b)
+{
+    a ^= b;
+    b ^= a;
+    a ^= b;
+    
+}
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers\n";
+    cin>>a>>b;
+    cout<<"Numbers before swapping"<<endl;
+    cout << "A is : " << a << endl;
+    cout << "B is : " << b << endl;
+    swapUsingBinary(a,b);
+    cout<<"Numbers after swapping"<<endl;
+    cout << "A is : " << a << endl;
+    cout << "B is : " << b << endl;
+}
