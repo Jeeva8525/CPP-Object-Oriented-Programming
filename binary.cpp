d8e7fa3e21df9453ceb55c8424aa279a770284c7
Jeeva8525 <jeevananthan8507@gmail.com>
2025-08-30 09:36:50 +0530

    remodified

--- binary.cpp
+++ binary.cpp
@@ -3,10 +3,10 @@ using namespace std;
 template<typename T>
 void swapUsingBinary(T& a,T& b)
 {
+    //swapping using bitwise xor operation 
     a ^= b;
     b ^= a;
     a ^= b;
-    
 }
 int main()
 {
